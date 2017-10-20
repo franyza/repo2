995b2ef9a05d5d9bcf02a80bfcc8cf78cd268939
Francisco <francisco@payapi.io>
2017-10-20 15:07:16 +0200

    no message

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-
-use \Franyza\Repo3\branding;
+require __DIR__."/vendor/autoload.php";
+use Franyza\Repo3\branding;
 
 $branding = new Branding();
 echo 'Hello from '.$branding->getBrandingCode();
